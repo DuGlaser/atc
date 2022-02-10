a4135d50ccc3ade76675384a90da85c1c3bec1a6
DuGlaser <duglaser0618@gmail.com>
2022-02-10 13:58:35 +0900

    :white_check_mark: Add the test cases of `Validate` method.

---
+++ internal/atc_test.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"fmt"
+	"testing"
+)
+
+func createEmptyError(key string) error {
+	return fmt.Errorf("Config.%s is empty.", key)
+}
+
+func TestValidate(t *testing.T) {
+	tests := []struct {
+		input  Config
+		expect error
+	}{
+		{input: Config{Cmd: "", Lang: "", FileName: "", Template: ""}, expect: createEmptyError("Cmd")},
+		{input: Config{Cmd: "Cmd", Lang: "", FileName: "", Template: ""}, expect: createEmptyError("Lang")},
+		{input: Config{Cmd: "Cmd", Lang: "Lang", FileName: "", Template: ""}, expect: createEmptyError("FileName")},
+		{input: Config{Cmd: "Cmd", Lang: "Lang", FileName: "FileName", Template: ""}, expect: nil},
+	}
+
+	for _, test := range tests {
+		err := test.input.Validate()
+		if err == nil && test.expect == nil {
+			continue
+		}
+
+		if !(err.Error() == test.expect.Error()) {
+			t.Errorf("config.Validate() return wrong value. got=%v expect=%v.", err, test.expect)
+		}
+	}
+}
