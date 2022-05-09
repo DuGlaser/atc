67e8c127ac8e1146d5e5259a81578f3394d43cf4
DuGlaser <duglaser0618@gmail.com>
2022-05-09 22:56:38 +0900

    :recycle: Refactoring.

--- internal/atc.go
+++ internal/atc.go
@@ -2,16 +2,6 @@ package internal
 
 // FIXME: ここにあるstructは役割が曖昧なので消したい
 
-type Contest struct {
-	Name string
-	Url  string
-}
-
-type Task struct {
-	ID   string
-	Path string
-}
-
 type Problem struct {
 	// URLに使われる、コンテスト内の問題を一意に識別するID
 	ID string
--- internal/repository/config/contest.go
+++ internal/repository/config/contest.go
@@ -3,23 +3,33 @@ package config
 import (
 	"fmt"
 
-	"github.com/DuGlaser/atc/internal"
 	"github.com/DuGlaser/atc/internal/core"
 	"github.com/DuGlaser/atc/internal/repository/fetcher"
 	"github.com/DuGlaser/atc/internal/repository/scraper"
 	"github.com/spf13/viper"
 )
 
+type Contest struct {
+	Name string
+	Url  string
+}
+
+type Task struct {
+	ID   string
+	Path string
+}
+
 type ContestConfig struct {
 	v       *viper.Viper
 	config  *core.Config
-	contest *internal.Contest
-	task    *internal.Task
+	contest *Contest
+	task    map[string]Task
 }
 
 func NewContestConfig() (*ContestConfig, error) {
 	cc := &ContestConfig{
-		v: viper.New(),
+		v:    viper.New(),
+		task: map[string]Task{},
 	}
 	err := cc.load()
 	return cc, err
@@ -49,12 +59,12 @@ func (cc *ContestConfig) ReadConfig() (*core.Config, error) {
 	return &config, nil
 }
 
-func (cc *ContestConfig) ReadContestSetting() (*internal.Contest, error) {
+func (cc *ContestConfig) ReadContestSetting() (*Contest, error) {
 	if cc.contest != nil {
 		return cc.contest, nil
 	}
 
-	var contest internal.Contest
+	var contest Contest
 	err := cc.v.UnmarshalKey("contest", &contest)
 	if err != nil {
 		return nil, err
@@ -65,12 +75,12 @@ func (cc *ContestConfig) ReadContestSetting() (*internal.Contest, error) {
 	return &contest, nil
 }
 
-func (cc *ContestConfig) ReadTaskSetting(displayID string) (*internal.Task, error) {
-	if cc.task != nil {
-		return cc.task, nil
+func (cc *ContestConfig) ReadTaskSetting(displayID string) (*Task, error) {
+	if task, ok := cc.task[displayID]; ok {
+		return &task, nil
 	}
 
-	var task internal.Task
+	var task Task
 	err := cc.v.UnmarshalKey(fmt.Sprintf("tasks.%s", displayID), &task)
 	if task.Path == "" && task.ID == "" {
 		return nil, fmt.Errorf("Not found task %s.", displayID)
@@ -80,13 +90,17 @@ func (cc *ContestConfig) ReadTaskSetting(displayID string) (*internal.Task, erro
 		return nil, err
 	}
 
-	cc.task = &task
-
+	cc.task[displayID] = task
 	return &task, nil
 }
 
 // FIXME: configの中にfetchがあるのは違和感がある
 func (cc *ContestConfig) SetTaskID(displayID string) error {
+	task, err := cc.ReadTaskSetting(displayID)
+	if err != nil {
+		return err
+	}
+
 	contest, err := cc.ReadContestSetting()
 	if err != nil {
 		return err
@@ -112,7 +126,7 @@ func (cc *ContestConfig) SetTaskID(displayID string) error {
 		return err
 	}
 
-	cc.task.ID = p.ID
+	task.ID = p.ID
 
 	return nil
 }
