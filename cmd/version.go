28be740f165527e8ed92208f3a017a44dd5f71dd
DuGlaser <duglaser0618@gmail.com>
2022-07-03 20:02:21 +0900

    :recycle: Change the version output.

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of atc.",
 	Run: func(_ *cobra.Command, args []string) {
-		fmt.Println(version)
+		fmt.Print(version)
 	},
 }
 
