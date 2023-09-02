15316191a95555ede0d5f9630a7c658dd368dcef
DuGlaser <duglaser0618@gmail.com>
2023-09-02 16:51:25 +0900

    :recycle: Improve fetcher logging.

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/henvic/httpretty v0.1.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- internal/repository/fetcher/fetcher.go
+++ internal/repository/fetcher/fetcher.go
@@ -2,31 +2,45 @@ package fetcher
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os"
 	"path"
 	"strings"
 	"time"
 
+	"github.com/DuGlaser/atc/internal"
 	"github.com/DuGlaser/atc/internal/auth"
 	"github.com/DuGlaser/atc/internal/repository/scraper"
+	"github.com/henvic/httpretty"
 )
 
 const ATCODER_URL = "https://atcoder.jp/"
 
-func getDefaultClient() *http.Client {
-	return &http.Client{
-		Transport: &http.Transport{
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-			}).DialContext,
-			TLSHandshakeTimeout: 10 * time.Second,
-		},
-		Timeout: 30 * time.Second}
+var logger = &httpretty.Logger{
+	Time:            true,
+	TLS:             true,
+	RequestHeader:   true,
+	RequestBody:     true,
+	ResponseHeader:  true,
+	ResponseBody:    true,
+	Colors:          false,
+	Formatters:      []httpretty.Formatter{&httpretty.JSONFormatter{}},
+	MaxResponseBody: 10000,
+}
 
+func getDefaultClient() *http.Client {
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	if internal.Verbose {
+		logger.SetOutput(os.Stderr)
+		client.Transport = logger.RoundTripper(http.DefaultTransport)
+	}
+
+	return client
 }
 
 func setCookie(req *http.Request) error {
@@ -40,7 +54,7 @@ func setCookie(req *http.Request) error {
 }
 
 func is2xx(res *http.Response) bool {
-	return res.StatusCode >= http.StatusOK && res.StatusCode < http.StatusMultipleChoices
+	return 200 <= res.StatusCode && res.StatusCode < 300
 }
 
 func GetAtcoderUrl(p ...string) string {
@@ -224,27 +238,36 @@ func PostProblemAnswer(contest, problem, lang, code string) (*http.Response, err
 
 	submitUrl := GetAtcoderUrl("contests", contest, "submit")
 
-	req, err := http.NewRequest("GET", submitUrl, nil)
-	if err != nil {
-		return nil, err
+	getCSRFToken := func() (csrf string, err error) {
+		req, err := http.NewRequest("GET", submitUrl, nil)
+		if err != nil {
+			return csrf, err
+		}
+
+		if err := setCookie(req); err != nil {
+			return csrf, err
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			return csrf, err
+		}
+		defer res.Body.Close()
+
+		sp, err := scraper.NewSubmitPage(res.Body)
+		if err != nil {
+			return csrf, err
+		}
+
+		csrf, err = sp.GetCSRFToken()
+		if err != nil {
+			return csrf, err
+		}
+
+		return csrf, err
 	}
 
-	if err := setCookie(req); err != nil {
-		return nil, err
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	sp, err := scraper.NewSubmitPage(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	csrf, err := sp.GetCSRFToken()
+	csrf, err := getCSRFToken()
 	if err != nil {
 		return nil, err
 	}
